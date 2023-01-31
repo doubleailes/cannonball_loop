2173c4584a11fe0bbc0da2f53382d9d37a008026
Philippe Llerena <philippe.llerena@forticheprod.com>
2023-01-31 11:54:51 +0100

    init commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+extern crate photon_rs;
+use photon_rs::multiple::watermark;
+use photon_rs::native::{open_image, save_image};
+use photon_rs::PhotonImage;
+
+fn get_center(x: u32, y: u32) -> (u32, u32) {
+    (x / 2, y / 2)
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    //Open the stamp image
+    let stamp: PhotonImage = open_image("samples/stamp.png")?;
+    // Open the image (a PhotonImage is returned)
+    let mut img: PhotonImage = open_image("samples/1280px-EgliseDeClesles.jpg")?;
+    //Get center of the image
+    let (x, y) = get_center(img.get_width(), img.get_height());
+    //Get center of the stamp
+    let (x_stamp, y_stamp) = get_center(stamp.get_width(), stamp.get_height());
+    // Watermark the image
+    watermark(&mut img, &stamp, x - x_stamp, y - y_stamp);
+
+    // Write file to filesystem.
+    save_image(img, "sample/raw-1280px-EgliseDeClesles.jpg");
+
+    Ok(())
 }
